d565176e1c7889598460e3573e151d40d09ed748
Quyen Kieu <9664014+quyenhuukieu@users.noreply.github.com>
2026-06-16 06:08:06 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MyTrip from './MyTrip'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <MyTrip completedTrips={[]} incompleteTrips={[]} />
+    </>
+  );
+}
+
+export default App
---
+++ src/MyTrip.jsx
@@ -0,0 +1,12 @@
+export default function MyTrip({ completedTrips, incompleteTrips }) {
+    return (
+        <div>
+            <h1>My Trips</h1>
+            <p>New trips form will go here</p>
+            <h3>Completed Trips:</h3>
+
+            <h3>Incomplete Trips:</h3>
+            
+        </div>
+    )
+}
